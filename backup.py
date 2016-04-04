244f5817a4a00d17b6a58a477ab53117c8664ac4
Kyle Luce <kylegluce@gmail.com>
2016-04-03 20:25:32 -0500

    adding backup.py

---
+++ backup.py
@@ -0,0 +1,11 @@
+#
+#        _   ,--()            Make sure you back it up!
+#       ( )-'-.------|>
+#        "     `--[]          github.com/nomadmtb/backup_scripts.py
+#
+
+# Kyle Luce
+# 4/3/2016
+# backup.py
+# DESC: This is the module that will contain all of the backup logic.
+#------------------------------------------------------------------------------
